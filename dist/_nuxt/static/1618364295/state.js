3221a6757ab56c711afd83fb9d5fcc04c92f98d3
Fendi <do1fendi@gmail.com>
2021-04-14 09:39:11 +0800

    gh-pages installed

---
+++ dist/_nuxt/static/1618364295/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fsiloah-cruise-booking\u002F_nuxt\u002Fstatic\u002F1618364295",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fsiloah-cruise-booking\u002F",assetsPath:"\u002Fsiloah-cruise-booking\u002F_nuxt\u002F",cdnURL:a}}}}(null));
