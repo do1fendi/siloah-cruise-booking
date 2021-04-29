e5f0eb2ebffcf4b24b4b6d3d20af4aaa50559409
Fendi <do1fendi@gmail.com>
2021-04-29 09:45:49 +0800

    added agreement download

--- components/TravelerForm.vue
+++ components/TravelerForm.vue
@@ -165,7 +165,7 @@
       </b-card>
     </div>
     <div class="mt-2 text-right">
-      <a href='/cruiseAgreement.docx' download>
+      <a href='/siloah-cruise-booking/cruiseAgreement.docx' download>
       <h5>
         <b-badge
           class="showAgreement"
